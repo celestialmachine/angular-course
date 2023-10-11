6d34950c6b80594233735411d17dae237a563b4b
Victor Diaz <master_v12@hotmail.com>
2023-10-10 20:12:48 -0700

    Complete Angular 4 beginners course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {HttpParams} from '@angular/common/http';
+import {CoursesService} from './services/courses.service';
 
 @Component({
   selector: 'app-root',
@@ -16,15 +17,19 @@ export class AppComponent implements OnInit {
 
   courses$: Observable<Course[]>;
 
-  constructor(private http: HttpClient) {
+  constructor(private coursesService: CoursesService) {
 
   }
 
   ngOnInit() {
-    const params = new HttpParams()
-      .set("page", "1")
-      .set("pageSize", "10");
-    this.courses$ = this.http.get<Course[]>('/api/courses', {params});
+    this.courses$ = this.coursesService.loadCourses();
+  }
+
+  save(course: Course) {
+    this.coursesService.saveCourse(course)
+      .subscribe(
+        () => console.log('Course Saved!')
+        );
   }
 
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { HttpClient } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+
+  loadCourses(): Observable<Course[]> {
+    const params = new HttpParams()
+      .set("page", "1")
+      .set("pageSize", "10");
+
+      return this.http.get<Course[]>('/api/courses', {params});
+  }
+
+  saveCourse(course:Course) {
+
+    const header = new HttpHeaders()
+      .set("X-Auth", "userId");
+
+    return this.http.put(`/api/courses/${course.id}`,
+      course,
+      {headers: header});
+  }
 }
